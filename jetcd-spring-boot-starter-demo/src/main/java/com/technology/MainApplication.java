2bb012bae1fe448ad2a3789a9f2f0d29dea68c2b
徐雨佳 <x52technology@foxmail.com>
2019-08-18 14:48:52 +0800

    小黑8.18写的代码2--commit3   小黑的功能模块合道主线

--- jetcd-spring-boot-starter-demo/src/main/java/com/technology/MainApplication.java
+++ jetcd-spring-boot-starter-demo/src/main/java/com/technology/MainApplication.java
@@ -27,6 +27,7 @@ public class MainApplication implements CommandLineRunner {
         System.out.println(environment.getProperty("etcd.config.enabled"));
         System.out.println(environment.getProperty("etcd.config.endpoints"));
         System.out.println(jetcdDistributedLock);
-        // TODO 小黑8.18写的代码
+        // TODO 小黑8.18写的代码3--commit3
+        // commit1 tes3
     }
 }
